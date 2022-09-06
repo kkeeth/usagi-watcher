26c4a0047f3be76be89c7854897fd718c94ff784
kkeeth <zensin0082@gmail.com>
2022-09-07 02:25:42 +0900

    feat: add  1 option of authorize

--- functions/src/slack/app.ts
+++ functions/src/slack/app.ts
@@ -1,18 +1,27 @@
-import * as functions from "firebase-functions";
 import {App, ExpressReceiver} from "@slack/bolt";
 import * as UsagiReferee from "../modules/UsagiReferee";
+import { config } from "dotenv";
+config()
 
-const config = functions.config();
+const authorizeFn = async () => {
+  return {
+    botToken: process.env.SLACK_BOT_TOKEN,
+    botId: process.env.SLACK_BOT_ID,
+  };
+
+  throw new Error("No matching authorizations");
+};
 
 export const expressReceiver = new ExpressReceiver({
-  signingSecret: config.slack.secret,
+  signingSecret: process.env.SLACK_SIGNING_SECRET || "",
   endpoints: "/events",
   processBeforeResponse: true,
 });
 
 const app = new App({
   receiver: expressReceiver,
-  token: config.slack.token,
+  token: process.env.SLACK_BOT_TOKEN,
+  authorize: authorizeFn,
 });
 
 app.message(UsagiReferee.makeRegexp(), async ({message, client}) => {
